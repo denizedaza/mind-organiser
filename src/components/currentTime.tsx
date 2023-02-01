b6b7897293c2f13101d1e95d225fc67701186584
denizedaza <dazadenize@gmail.com>
2023-02-01 16:36:43 -0500

    separate logic from basic clock display to the future clock

--- src/components/currentTime.tsx
+++ src/components/currentTime.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Time } from "./types";
 import FutureTime from "./futureTime";
+import { padDisplay } from "@/utils";
 
 const CurrentTime = () => {
   const [currentDate, setCurrentDate] = useState<Date>(new Date());
@@ -12,27 +13,22 @@ const CurrentTime = () => {
     day: currentDate.getDate(),
     year: currentDate.getFullYear(),
   });
-  const [isRunning, setIsRunning] = useState(true);
-
-  function padDisplay(value: number) {
-    return value.toString().padStart(2, "0");
-  }
-
-  function updateDate() {
-    let date = new Date();
-    setCurrentDate(date);
-  }
-
-  function updateTime() {
-    setCurrentTime({
-      ...currentTime,
-      hour: currentDate.getHours(),
-      minute: currentDate.getMinutes(),
-      seconds: currentDate.getSeconds(),
-    });
-  }
 
   useInterval(() => {
+    function updateDate() {
+      let date = new Date();
+      setCurrentDate(date);
+    }
+
+    function updateTime() {
+      setCurrentTime({
+        ...currentTime,
+        hour: currentDate.getHours(),
+        minute: currentDate.getMinutes(),
+        seconds: currentDate.getSeconds(),
+      });
+    }
+
     updateDate();
     updateTime();
   }, 1000);
@@ -52,25 +48,6 @@ const CurrentTime = () => {
     "December",
   ];
 
-  function handleIsRunningChange(e: {
-    target: { checked: boolean | ((prevState: boolean) => boolean) };
-  }) {
-    setIsRunning(e.target.checked);
-  }
-
-  function calcFutureTime(minutesInFuture: number) {
-    const startHour = currentTime.hour;
-    const startMinute = currentTime.minute;
-    const TBDMinutes = (startMinute + minutesInFuture ?? 25) % 60;
-    const TBDHour = TBDMinutes <= 24 ? startHour + 1 : startHour;
-    const finishedTime = {
-      hours: TBDHour,
-      minutes: TBDMinutes,
-      // TODO, future: add checks if user is working past midnight and the day/month/year rolls over. start with day then month, etc
-    };
-    return finishedTime;
-  }
-
   function useInterval(callback: () => void, delay: number) {
     const savedCallback = useRef();
 
@@ -84,7 +61,7 @@ const CurrentTime = () => {
       }
       let id = setInterval(tick, delay);
       return () => clearInterval(id);
-    });
+    }, []);
   }
 
   return (
@@ -93,23 +70,15 @@ const CurrentTime = () => {
       <h3>{`${monthNames[currentTime?.month]} ${currentTime?.day}, ${
         currentTime?.year
       }`}</h3>
-      <h1>{`${padDisplay(currentTime?.hour)}:${padDisplay(
-        currentTime?.minute
-      )}:${padDisplay(currentTime?.seconds)}`}</h1>
-      <h1>{currentDate.toLocaleTimeString()}</h1>
+      <h1>
+        currentTime obj-
+        {`${padDisplay(currentTime?.hour)}:${padDisplay(
+          currentTime?.minute
+        )}:${padDisplay(currentTime?.seconds)}`}
+      </h1>
+      <h1>currentDate obj- {currentDate.toLocaleTimeString()}</h1>
       <br />
-      {/* <input
-        type="checkbox"
-        checked={isRunning}
-        onChange={handleIsRunningChange}
-      />
-      Start Now? */}
-      <p>Predicted End Time:</p>
-      <FutureTime currentTime={currentTime} />
-      {/* <h3>
-        {padDisplay(calcFutureTime(25).hours)}:
-        {padDisplay(calcFutureTime(25).minutes)}
-      </h3> */}
+      <FutureTime currentTime={currentTime} displayCallback={padDisplay} />
     </div>
   );
 };
--- src/components/futureTime.tsx
+++ src/components/futureTime.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Time } from "./types";
 
-const FutureTime = (props: { currentTime: Time }) => {
+const FutureTime = (props: {
+  currentTime: Time;
+  displayCallback: Function;
+}) => {
+  const { currentTime, displayCallback } = props;
+  const [hasStarted, setHasStarted] = useState(false); //if future clock has started
+
+  function handleHasStartedChange(e: {
+    target: { checked: boolean | ((prevState: boolean) => boolean) };
+  }) {
+    setHasStarted(e.target.checked);
+  }
+
+  function calcFutureTime(minutesInFuture: number) {
+    const startHour = currentTime.hour;
+    const startMinute = currentTime.minute;
+
+    const TBDMinutes = (startMinute + minutesInFuture ?? 25) % 60;
+    const TBDHour = TBDMinutes <= 24 ? startHour + 1 : startHour;
+    const finishedTime = {
+      hours: TBDHour,
+      minutes: TBDMinutes,
+      // TODO, future: add checks if user is working past midnight and the day/month/year rolls over. start with day then month, etc
+    };
+    return finishedTime;
+  }
+
+  const displayTime = `${displayCallback(
+    calcFutureTime(25).hours
+  )}:${displayCallback(calcFutureTime(25).minutes)}`;
+
   return (
     <>
-      <div> End TIME:</div>
-      <div>
-        {props.currentTime.hour}:{props.currentTime.minute}:
-        {props.currentTime.seconds}
-      </div>
+      <p>Predicted End Time:</p>
+      <h3>{displayTime}</h3>
     </>
   );
 };
